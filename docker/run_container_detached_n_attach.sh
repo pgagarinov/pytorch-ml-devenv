7b2b09c1d36468602827ab42216ee3dee9d1ac77
Peter Gagarinov <pgagarinov@gmail.com>
2021-03-28 20:51:49 +0300

    ADD: pass AWS_PROFILE environment variable into the container

--- docker/run_container_detached_n_attach.sh
+++ docker/run_container_detached_n_attach.sh
@@ -7,5 +7,6 @@ container_id=`docker run -it -d --gpus all --shm-size=$IN_CONTAINER_SHARED_MEMOR
    --mount type=bind,source=$HOME/.aws,target=/home/$in_container_user_name/.aws,readonly \
    --mount type=bind,source=$HOME/.ssh,target=/home/$in_container_user_name/.ssh,readonly \
    --mount type=bind,source=$HOME/.kaggle,target=/home/$in_container_user_name/.kaggle,readonly \
+   -e AWS_PROFILE=$AWS_PROFILE \
    $image_id /bin/bash`
 docker exec -it $container_id /bin/bash
